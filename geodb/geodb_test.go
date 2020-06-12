b7ee3c3b1b42fe84ca8e7c2b148562de866b29c7
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-06-12 22:22:49 +0200

    add geodb test

---
+++ geodb/geodb_test.go
@@ -0,0 +1,12 @@
+package geodb
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	g := New()
+	if g.Filepath != "/var/tmp/GeoLite2-City.mmdb" {
+		t.Errorf("default geodb path is wrong: %s", g.Filepath)
+	}
+}
