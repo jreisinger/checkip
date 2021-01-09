42f2d182750dca4af590a2c2e72eeec8fd653682
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-01-09 13:04:38 +0100

    improve ASN tests

--- asn/asn_test.go
+++ asn/asn_test.go
@@ -9,18 +9,23 @@ func TestForIP(t *testing.T) {
 	type testpair struct {
 		ip          string
 		countryCode string
+		description string
 	}
 	testpairs := []testpair{
-		{"1.1.1.1", "US"},
-		{"8.8.8.8", "US"},
+		{"1.1.1.1", "US", "CLOUDFLARENET - Cloudflare, Inc."},
+		{"8.8.8.8", "US", "GOOGLE - Google LLC"},
 	}
 	for _, tp := range testpairs {
 		a := New()
 		ip := net.ParseIP(tp.ip)
 		err := a.ForIP(ip)
 		if a.CountryCode != tp.countryCode || err != nil {
-			t.Errorf("country code for %s is expected to be %s but is %s with %v",
+			t.Errorf("country code for %s was expected to be '%s' but is '%s' with %v",
 				tp.ip, tp.countryCode, a.CountryCode, err)
+		}
+		if a.Description != tp.description || err != nil {
+			t.Errorf("description for %s was expected to be '%s' but is '%s' with %v",
+				tp.ip, tp.description, a.Description, err)
 		}
 	}
 }
