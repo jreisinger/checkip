8219bd16994d55a8998475a3bf891d8c1a951888
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-01-25 13:41:19 +0100

    minor changes in flags.go

--- flags.go
+++ flags.go
@@ -25,7 +25,7 @@ func ParseFlags() (Flags, error) {
 	f := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 
 	f.BoolVar(&flags.Version, "version", false, "print version")
-	f.Var(&flags.ChecksToRun, "check", "run `check[,...]` instead of all checks")
+	f.Var(&flags.ChecksToRun, "check", "run only `<check>[,<check>,...]` instead of all checks")
 
 	f.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "%s [flags] <ipaddr>\n", os.Args[0])
