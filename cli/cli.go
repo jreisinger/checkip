e354e6b567d3e48f9187aa0c464e2b847f7cf622
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-20 15:50:18 +0200

    add malicious_prob to JSON output

--- cli/cli.go
+++ cli/cli.go
@@ -45,12 +45,15 @@ func (rs Results) PrintJSON(ipaddr net.IP) {
 	// if len(rs) == 0 {
 	// 	return
 	// }
+	_, _, prob := rs.maliciousStats()
 	out := struct {
-		IpAddr net.IP  `json:"ipaddr"`
-		Check  Results `json:"checks"`
+		IpAddr        net.IP  `json:"ipaddr"`
+		Check         Results `json:"checks"`
+		MaliciousProb float64 `json:"malicious_prob"`
 	}{
 		ipaddr,
 		rs,
+		prob,
 	}
 	enc := json.NewEncoder(os.Stdout)
 	if err := enc.Encode(out); err != nil {
