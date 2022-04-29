35875ef5714144db2e0c205959bedf0aec34b3c0
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-29 16:28:41 +0200

    test downloadFile

--- check/file_test.go
+++ check/file_test.go
@@ -1,9 +1,14 @@
 package check
 
 import (
+	"io"
+	"net/http"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetDbFilesPath(t *testing.T) {
@@ -41,4 +46,22 @@ func TestIsOlderThanOneWeek(t *testing.T) {
 			t.Fatalf("test case %d: got %t expected %t", i+1, got, tc.olderThanOneWeek)
 		}
 	}
+}
+
+func TestDownloadFile(t *testing.T) {
+	t.Run("given valid response then result and no error is returned", func(t *testing.T) {
+		handlerFn := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+			rw.Write(loadResponse(t, "file.txt"))
+		})
+
+		testUrl := setMockHttpClient(t, handlerFn)
+		rc, err := downloadFile(testUrl)
+		require.NoError(t, err)
+		defer rc.Close()
+
+		b, err := io.ReadAll(rc)
+		require.NoError(t, err)
+		assert.Equal(t, "just a simple file", string(b))
+	})
 }
